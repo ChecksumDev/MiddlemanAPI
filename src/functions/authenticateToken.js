f64b8891fe10dbfd2764a54fa909fd18cb454cc4
Checksum <collier.bell20@gmail.com>
2021-04-11 20:08:08 -0500

    IP Logging

--- src/functions/authenticateToken.js
+++ src/functions/authenticateToken.js
@@ -1,11 +1,20 @@
 let { Apikeys } = require('./database');
+const { debug } = require('./logger');
 
 async function authenticateToken(req, res, next) {
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     let authHeader = req.headers['authorization'];
     let token = authHeader && authHeader.split(' ')[1];
-    if (token == null) return res.sendStatus(401);
+    if (token == null) {
+        debug(`${ip} sent an unauthorized request to the API.`)
+        return res.sendStatus(401);
+    }
     await Apikeys.findOne({ where: { key: `${token}` } }).then((encounter) => {
-        if (!encounter) return res.status(401).send("Unauthorized");
+        if (!encounter) {
+            debug(`${ip} sent an unauthorized request to the API.`)
+            return res.status(401).send("Unauthorized");
+        }
+        debug(`${ip} sent an authorized request to the API.`)
         return next();
     });
 }
